fae2b13181ed4d3eef3b681940d0cba0019d7757
ababoi4 <105204537+ababoi4@users.noreply.github.com>
2022-08-15 17:51:02 +0300

    Add Example002

---
+++ Example002/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 2
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+int aX = 3;
+int aY = 6;
+int aZ = 8;
+int bX = 2;
+int bY = 1;
+int bZ = -7;
+
+double result = Math.Sqrt((bX - aX)*(bX - aX) + (bY - aY)*(bY - aY) + (bZ - aZ)*(bZ - aZ));
+System.Console.WriteLine(result);
