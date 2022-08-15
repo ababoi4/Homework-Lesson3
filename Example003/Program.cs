ef5a1c1901a841400aac3170b33c7983609bf8d9
ababoi4 <105204537+ababoi4@users.noreply.github.com>
2022-08-15 18:15:22 +0300

    Add Example003

---
+++ Example003/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 3
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readVelue = Console.ReadLine();
+    int result = int.Parse(readVelue);
+    return result;
+}
+
+
+void SetN(int num)
+{
+    int i=1;
+    while(i<=num) 
+    {
+        System.Console.Write(i*i*i + " ");
+        i++;
+    }
+}
+
+
+SetN(Prompt("Введите число -> "));
